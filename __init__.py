08c5bc7c215bcf18246b87681a2d45ed2c1b8074
Jochem Vaartjes <jochem@jochem.me>
2021-10-05 05:05:14 +0000

    update __init__

--- __init__.py
+++ __init__.py
@@ -1,20 +1,52 @@
 #!/usr/bin/env python3
-import aiohttp
-import asyncio
 import requests
 import json
 import argparse
+import logging
 
 import environment
 import cbdauth
 
-## Currently only returns json information on the default organisation
-def get_organisations():
-    token = cbdauth.getToken(keyid=environment.keyid,
+from dataclasses import dataclass
+
+@dataclass
+class CiscoBDOrganisationClass:
+    """Cisco Business Dashboard Organisation Class"""
+
+    id: str
+    name: str
+    description: str
+    defaultgroup: str
+    networkcount: int
+    devicecount: int
+    monitorprofiles: str
+    changewindowtype: str
+    changewindow: str
+
+    @staticmethod
+    def from_json(item):
+        return CiscoBDOrganisationClass(
+            id=item["id"],
+            name=item["name"],
+            description=item["description"],
+            defaultgroup=item["default-group"],
+            networkcount=item["network-count"],
+            devicecount=item["device-count"],
+            monitorprofiles=item["monitor-profiles"],
+            changewindowtype=item["change-window-type"],
+            changewindow=item["change-window"],
+        )
+
+DEFAULT_SOURCE = CiscoBDOrganisationClass
+
+#Create Token
+token = cbdauth.getToken(keyid=environment.keyid,
                             secret=environment.secret,
                             clientid=environment.clientid,
                             appname=environment.appname)
 
+## Currently only returns json information on the default organisation
+def get_default_organisation(source=DEFAULT_SOURCE):
     try:
         # Build and send the API request.  The getOrganizations API path is
         # /api/v2/orgs
@@ -32,10 +64,14 @@ def get_organisations():
     else:
         if response.status_code == 200:
             organisations = response.json()
+
             for organisation in organisations['data']:
                 if 'default' in organisation:
-                    print("Default org:",organisation["name"])
-                    results = organisation
+                    results = []
+                    try:
+                        results.append(source.from_json(organisation))
+                    except KeyError:
+                        logging.getLogger(__name__).warning("Got wrong data: %s", item)
         else:
             # Some other error occurred.
             print('HTTPError:',response.status_code,response.headers)
@@ -46,6 +82,6 @@ def get_organisations():
                 print(json.dumps(response.json(),indent=2))
     return results
 
-org = get_organisations()
+org = get_default_organisation()
 print(org)
 
